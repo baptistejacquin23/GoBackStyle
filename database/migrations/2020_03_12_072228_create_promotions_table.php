0c3e8c71425c61af8f9ac782ae96ac1569ac3888
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-03-12 08:40:36 +0100

    Core: Add migration files for create tables in database

---
+++ database/migrations/2020_03_12_072228_create_promotions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('code_id')->index();
+            $table->timestamps();
+        });
+
+        Schema::table('promotions', function($table) {
+            $table->foreign('code_id')->references('id')->on('codes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('promotions', function (Blueprint $table) {
+            $table->dropForeign(['code_id']);
+        });
+
+        Schema::dropIfExists('promotions');
+    }
+}
