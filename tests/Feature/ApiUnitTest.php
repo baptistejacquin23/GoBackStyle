27f1f2a60f7e41ddfa2bd44143286c0193de811c
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-03-12 15:47:17 +0100

    Feat: Creation of Unit Tests for the Api

---
+++ tests/Feature/ApiUnitTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Code;
+use App\Promotion;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiUnitTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+
+    public function testApiGetPromotions()
+    {
+        putenv('DB_DEFAULT=sqlite_testing');
+        $code = new Code();
+        $code->id = 1;
+        $code->name = "test";
+        $savedCode = $code->save();
+        $this->assertTrue($savedCode);
+
+        $promotion = new Promotion();
+        $promotion->discount = "-20%";
+        $promotion->description = "description";
+        $promotion->link = "link";
+        $promotion->image_path = "image_path";
+        $promotion->validate_start_date = \date("Y-m-d");
+        $promotion->validate_end_date = \date('Y-m-d', strtotime("+1 day"));
+        $promotion->code_id = 1;
+        $savedPromotion =  $promotion->save();
+        $this->assertTrue($savedPromotion);
+
+        $response = $this->json('GET', '/api/promotions/test');
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            "discount" => "-20%",
+            "description" => "description",
+            "image_path" => "image_path",
+            "link" => "link",
+            "validate_start_date" => \date("Y-m-d"),
+            "validate_end_date" => \date('Y-m-d', strtotime("+1 day")),
+        ]);
+    }
+
+    public function testApiCodeExistButNoPromotions()
+    {
+        putenv('DB_DEFAULT=sqlite_testing');
+        $code = new Code();
+        $code->name = "test";
+        $savedCode = $code->save();
+        $this->assertTrue($savedCode);
+
+        $response = $this->json('GET', '/api/promotions/test');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonFragment([
+            "error" => "Aucune promotion pour ce code"
+            ]);
+    }
+
+    public function testApiCodeDoesntExist()
+    {
+        putenv('DB_DEFAULT=sqlite_testing');
+
+        $response = $this->json('GET', '/api/promotions/test');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonFragment([
+            "error" => "Code promo non connu"
+        ]);
+    }
+}
