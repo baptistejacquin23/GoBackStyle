a7459d5ce2910ca37ed536d0806dd654465eff88
Baptiste Jacquin <baptiste.jacquin@campus-numerique-in-the-alps.com>
2020-03-12 08:47:03 +0100

    Feat: Creation of Api for return promotions

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Code;
+use App\Promotion;
+
+class ApiController extends Controller
+{
+    public function findPromotions($codeParam){
+        try {
+            $code = Code::where('name', $codeParam)->first();
+            if ($code != null){
+                $validPromotions = Promotion::where("codes_id", $code->id )->get();
+                if (count($validPromotions) !== 0){
+                    return $validPromotions;
+                }else{
+                    return response()->json([
+                        'error' => 'Aucune promotion pour ce code',
+                    ]);
+                }
+            }else{
+                return response()->json([
+                    'error' => 'Code promo non connu',
+                ]);
+            }
+        }catch (\Exception $e){
+            $message = null;
+            return $e;
+        }
+    }
+}
